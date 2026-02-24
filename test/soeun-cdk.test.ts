0d8f71f2afa56f483a697ae1b9ea694fd254903a
saokiritori <soeun-lee@nhnad.com>
2026-02-25 08:33:41 +0900

    [init] CDK 초기 설정

---
+++ test/soeun-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SoeunCdk from '../lib/soeun-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/soeun-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SoeunCdk.SoeunCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
